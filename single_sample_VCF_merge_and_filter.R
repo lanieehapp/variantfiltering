9703ef64430b206dc8433d6dc64c1b2e77053a6b
lanieehapp <lanieehapp@gmail.com>
2020-08-26 14:52:10 -0700

    added fai file as argument

--- single_sample_VCF_merge_and_filter.R
+++ single_sample_VCF_merge_and_filter.R
@@ -15,6 +15,7 @@ path<-c(args[1], args[2], args[3])
 dna_bam<-args[4]
 rna_bam<-args[5]
 ref<-args[6]
+ref_fai<-args[7]
 
 
 source("single_sample_VCF_merge_functions.R")
@@ -30,15 +31,15 @@ all_filt_variants<-merge_validation(rna_bam, ref, single.sample.merged, samp)
 
 single.sample.all<-cbind(single.sample.merged[[1]], single.sample.merged[[2]], single.sample.merged[[3]])
 
-write.csv(single.sample.all, file=args[7])
+write.csv(single.sample.all, file=args[8])
 
 #single.sample.filt<-cbind(single.sample.merged[[4]], single.sample.merged[[5]], single.sample.merged[[6]])
 save(all_filt_variants, file="all_filt_variants.RData")
-write.csv(all_filt_variants, file=args[8])
+write.csv(all_filt_variants, file=args[9])
 
 save(all_whitelist, file="all_whitelist.RData")
 
-write.csv(all_whitelist, file=args[9])
+write.csv(all_whitelist, file=args[10])
 
 save(single.sample.merged, file=args[10])
 
