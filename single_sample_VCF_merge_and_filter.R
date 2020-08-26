1b40d744be486d36ca0263d87e12e25db7867617
lanieehapp <lanieehapp@gmail.com>
2020-08-26 15:24:02 -0700

    debugging test

--- single_sample_VCF_merge_and_filter.R
+++ single_sample_VCF_merge_and_filter.R
@@ -17,31 +17,34 @@ rna_bam<-args[5]
 ref<-args[6]
 ref_fai<-args[7]
 
+comm<-paste0('parallel --colsep "\t" samtools mpileup -a -l filt_bed.txt --fasta-ref ',ref, ' ', rna_bam, ' -r {1} :::: ', ref_fai ,' > rna_filt.txt' )
+print(comm)
+
 
 source("single_sample_VCF_merge_functions.R")
 
-single.sample.merged<-three.caller.merge(samp, path)
-
-
-all_whitelist<-merge_whitelists(dna_bam, rna_bam, ref, single.sample.merged)
-
-all_filt_variants<-merge_validation(rna_bam, ref, single.sample.merged, samp)
-
-
-
-single.sample.all<-cbind(single.sample.merged[[1]], single.sample.merged[[2]], single.sample.merged[[3]])
-
-write.csv(single.sample.all, file=args[8])
-
-#single.sample.filt<-cbind(single.sample.merged[[4]], single.sample.merged[[5]], single.sample.merged[[6]])
-save(all_filt_variants, file="all_filt_variants.RData")
-write.csv(all_filt_variants, file=args[9])
-
-save(all_whitelist, file="all_whitelist.RData")
-
-write.csv(all_whitelist, file=args[10])
-
-save(single.sample.merged, file=args[10])
-
-print(args[7])
-
+# single.sample.merged<-three.caller.merge(samp, path)
+# 
+# 
+# all_whitelist<-merge_whitelists(dna_bam, rna_bam, ref, single.sample.merged)
+# 
+# all_filt_variants<-merge_validation(rna_bam, ref, single.sample.merged, samp)
+# 
+# 
+# 
+# single.sample.all<-cbind(single.sample.merged[[1]], single.sample.merged[[2]], single.sample.merged[[3]])
+# 
+# write.csv(single.sample.all, file=args[8])
+# 
+# #single.sample.filt<-cbind(single.sample.merged[[4]], single.sample.merged[[5]], single.sample.merged[[6]])
+# save(all_filt_variants, file="all_filt_variants.RData")
+# write.csv(all_filt_variants, file=args[9])
+# 
+# save(all_whitelist, file="all_whitelist.RData")
+# 
+# write.csv(all_whitelist, file=args[10])
+# 
+# save(single.sample.merged, file=args[10])
+# 
+# print(args[7])
+# 
