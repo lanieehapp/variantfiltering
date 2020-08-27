e16a07d1dc33f2bc5d155323960a155be17a16c3
lanieehapp <lanieehapp@gmail.com>
2020-08-27 06:45:23 -0700

    testing over - back to original script

--- single_sample_VCF_merge_and_filter.R
+++ single_sample_VCF_merge_and_filter.R
@@ -21,40 +21,30 @@ print(ref)
 ref_fai=args[7]
 
 
-whitelist_path<-"filtered_whitelist_08032020.txt"
-comm<-paste0('parallel --colsep "\t" samtools mpileup -a -l ', whitelist_path, ' --fasta-ref ',ref, ' ', rna_bam, ' -r {1} :::: ', ref_fai ,' > /data/rna_whitelist.txt'  )
+source("single_sample_VCF_merge_functions.R")
 
-print(comm)
-system(comm)
+single.sample.merged<-three.caller.merge(samp, path)
 
 
-rna_whitelist<-read.csv(file="/data/rna_whitelist.txt", sep="\t", stringsAsFactors = FALSE)
+all_whitelist<-merge_whitelists(dna_bam, rna_bam, ref, single.sample.merged)
+
+all_filt_variants<-merge_validation(rna_bam, ref, single.sample.merged, samp)
 
 
-#source("single_sample_VCF_merge_functions.R")
 
-# single.sample.merged<-three.caller.merge(samp, path)
-# 
-# 
-# all_whitelist<-merge_whitelists(dna_bam, rna_bam, ref, single.sample.merged)
-# 
-# all_filt_variants<-merge_validation(rna_bam, ref, single.sample.merged, samp)
-# 
-# 
-# 
-# single.sample.all<-cbind(single.sample.merged[[1]], single.sample.merged[[2]], single.sample.merged[[3]])
-# 
-# write.csv(single.sample.all, file=args[8])
-# 
-# #single.sample.filt<-cbind(single.sample.merged[[4]], single.sample.merged[[5]], single.sample.merged[[6]])
-# save(all_filt_variants, file="all_filt_variants.RData")
-# write.csv(all_filt_variants, file=args[9])
-# 
-# save(all_whitelist, file="all_whitelist.RData")
-# 
-# write.csv(all_whitelist, file=args[10])
-# 
-# save(single.sample.merged, file=args[10])
-# 
-# print(args[7])
-# 
+single.sample.all<-cbind(single.sample.merged[[1]], single.sample.merged[[2]], single.sample.merged[[3]])
+
+write.csv(single.sample.all, file=args[8])
+
+#single.sample.filt<-cbind(single.sample.merged[[4]], single.sample.merged[[5]], single.sample.merged[[6]])
+save(all_filt_variants, file="all_filt_variants.RData")
+write.csv(all_filt_variants, file=args[9])
+
+save(all_whitelist, file="all_whitelist.RData")
+
+write.csv(all_whitelist, file=args[10])
+
+save(single.sample.merged, file=args[10])
+
+print(args[7])
+
