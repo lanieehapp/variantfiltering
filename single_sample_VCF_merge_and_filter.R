82a3d05fc2040c93b6f7fd87c07f834bce35041e
lanieehapp <lanieehapp@gmail.com>
2020-09-15 12:33:46 -0700

    changed write.csv to write.table to remove rownames from output file

--- single_sample_VCF_merge_and_filter.R
+++ single_sample_VCF_merge_and_filter.R
@@ -33,13 +33,13 @@ single.sample.all<-cbind(single.sample.merged[[1]], single.sample.merged[[2]], s
 
 
 #all variants in .csv
-write.csv(single.sample.all, file=args[8], rownames=FALSE)
+write.table(single.sample.all, file=args[8], rownames=FALSE, quote=FALSE, sep = ",")
 
 #filtered variants in .csv
-write.csv(all_filt_variants, file=args[9], row.names = FALSE)
+write.table(all_filt_variants, file=args[9], row.names = FALSE, quote = FALSE, sep=",")
 
 #whitelist variants in .csv
-write.csv(all_whitelist, file=args[10], row_names=FALSE)
+write.table(all_whitelist, file=args[10], row_names=FALSE, quote = FALSE, sep=",")
 
 
 #simple merging for all variants, filtered variants, whitelist variants
